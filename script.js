2717d13b888d8f40bb59b54a01166958ca376e8b
DanAlexCox <149510425+DanAlexCox@users.noreply.github.com>
2024-03-02 18:13:45 +0000

    navbar basket functionality

---
+++ script.js
@@ -0,0 +1,52 @@
+document.addEventListener("DOMContentLoaded", function(){
+    var page = document.querySelector("html");
+    var basketNav = document.getElementById("buttonbasket");
+    var basketNavView = document.getElementById("mybasketdropdown");
+
+    page.addEventListener("click", function(event){
+        if(basketNavView.style.display === "block"){
+            basketNavView.style.display = "none";
+            event.stopPropagation();
+        }
+    });
+
+    basketNav.addEventListener("mouseover", function(){
+        if(basketNavView.style.display !== "block"){
+            basketNavView.style.display = "block";
+        }
+    });
+});
+
+function priceChange(){
+    const min = document.getElementsByName('minprice');
+    const max = document.getElementsByName('maxprice');
+
+    if(min >= max){
+        document.getElementsByID("myPrices").innerHTML = "Min price needs to be smaller than max price.";
+    }
+}
+
+//stored search item gets put back into search bar
+function getSearch(){
+    localStorage.getItem('search');
+}
+
+function resetFilters() {
+    window.location.href = 'browse.php';
+}
+
+function notLoggedIn(){
+    alert(window.location('accountlogin.php'));
+}
+
+// Function to open the full popup menu
+function openPopupMenu() {
+    document.querySelector('.popup-menu-overlay').style.display = 'block';
+    document.querySelector('.popup-menu').style.display = 'block';
+}
+
+// Function to close the full popup menu
+function closePopupMenu() {
+    document.querySelector('.popup-menu-overlay').style.display = 'none';
+    document.querySelector('.popup-menu').style.display = 'none';
+}
