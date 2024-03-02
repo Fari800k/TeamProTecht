d3ccae6718bf26c6e7ff03592040b38e0a3625e3
georgesmartx01 <copilaugeorgeandrei87@gmail.com>
2024-03-02 11:01:56 +0000

    minor improvements made in stockview

--- stockview.php
+++ stockview.php
@@ -7,14 +7,25 @@
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="employee.css">
     <title>Stock View</title>
-</head>
-<body>
-<section id="sidebar">
+    <style>
+    .product-image {
+        width: 40%;
+        height: 30%;
+    }
+
+    .product-image img{
+        max-width: 100%;
+    }
+    </style>
+    </head>
+    <body>
+        <section id="sidebar">
           <a href="" class="brand">
             <span class="icon">
                 <img src="logo.png" alt="teamprotect logo">
             </span>
           </a>
+          
           <ul class="side-menu top">
             <li>
                 <a href="stockview.php">
@@ -22,28 +33,37 @@
                     <span class="text">Stock View</span>
                 </a>
             </li>
+            
             <li>
                 <a href="#">
                     <i class='bx bx-mail-send' ></i>
                     <span class="text">Confirm Orders</span>
                 </a>
-            </li>            <li>
+            </li>            
+            
+            <li>
                 <a href="pending_orders.php">
                     <i class='bx bxs-hourglass' ></i>
                     <span class="text">Pending Orders</span>
                 </a>
-            </li>            <li>
+            </li>            
+            
+            <li>
                 <a href="viewcontact.php">
                     <i class='bx bxs-hourglass' ></i>
                     <span class="text">contact forms</span>
                 </a>
-            </li>            <li>
+            </li>            
+            
+            <li>
                 <a href="#">
                     <i class='bx bxs-package' ></i>
                     <span class="text">Fulfilled Orders</span>
                 </a>
             </li>
-        </li>            <li>
+        </li>            
+        
+        <li>
             <a href="#">
                 <i class='bx bx-log-out'></i>
                 <span class="text">Logout</span>
@@ -65,22 +85,21 @@
             <th>Created At</th>
             <th>Updated At</th>
         </tr>
+        
         <?php
            $pdo = new PDO('mysql:host=localhost;dbname=cs2tp', 'root', '');
            $statement = $pdo->query("SELECT Item_ID, ItemName, ItemDesc, Price, Img, Location_ID, Created_at, Updated_at  FROM item");
-
+           
            foreach ($statement as $rows) {
-            print "<td>" . $rows['Item_ID'] . "</td>";
-            print "<td>" . $rows['ItemName'] . "</td>";
-            print "<td>" . $rows['ItemDesc'] . "</td>";
-            print "<td>" . $rows['Price'] . "</td>";
-            print "<td>" . "<img src='CSS/images/' class='product-image'>" . "</td>";
-            print "<td>" . $rows['Location_ID'] . "</td>";
-            print "<td>" . $rows['Created_at'] . "</td>";
-            print "<td>" . $rows['Updated_at'] . "</td></tr>";
-        }
-        
-?>
-    
+                echo "<td>" . $rows['Item_ID'] . "</td>";
+                echo "<td>" . $rows['ItemName'] . "</td>";
+                echo "<td>" . $rows['ItemDesc'] . "</td>";
+                echo "<td>" . "£". $rows['Price'] . "</td>";
+                echo "<td>" . "<div class='product-image'><img src='./img/'></div>". "</td>";
+                echo "<td>" . $rows['Location_ID'] . "</td>";
+                echo "<td>" . $rows['Created_at'] . "</td>";
+                echo "<td>" . $rows['Updated_at'] . "</td></tr>";
+            }
+            ?>
 </body>
 </html>
