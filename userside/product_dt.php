f4da5707972ddeacb48f592326e6819777eb34d2
georgesmartx01 <copilaugeorgeandrei87@gmail.com>
2024-03-10 12:33:20 +0000

    Layout for displaying product info is much improved. CSS included as well

--- userside/product_dt.php
+++ userside/product_dt.php
@@ -26,7 +26,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <title><?php echo $row["ItemName"]?></title>
     <link rel="stylesheet" href="CSS/navbar.css">
     <link rel="stylesheet" href="CSS/product_dt.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -34,44 +34,76 @@
 
 <body>
     <div class="row">
-        <div class= "col-md-5">
-            <h1><?php echo $row["ItemName"]?></h1>
-            <img src="CSS/images/<?php echo $row["Img"]; ?>" style="width: 25%; padding-bottom: 20px;" />
+        <h1><?php echo $row["ItemName"]. ""?></h1>
+        <div class="column">
+            <div class="card">
+                <img class="phone_image" src="CSS/images/<?php echo $row["Img"]; ?>"/>
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+                <p class="price"><b><?php echo "£" . $row["Price"]?></b></p>
+                <button class="add-to-basket"><b>Add to Basket</b></button>
+                <p class="stock">
+                    <?php
+                    /* Get Availability status*/
+                    $stock = $row["Stock"]; // get number of items in stock from database
+                    
+                    /* 
+                    * Validate availability status for specific item,
+                    * i.e. in stock (plenty available), limited (very few left) 
+                    * or out of stock
+                    */
+                    if ($stock == 0) {
+                        echo "<i id='cross' class='fa fa-times'></i>" . "Out of stock";
+                    } else if ($stock >= 1 and $stock <= 15) {
+                        echo "<i id='exclamation' class='fa fa-exclamation'></i>" . "Hurry before it is sold out!";
+                    } else {
+                        echo "<i id='tickbox' class='fa fa-check'></i>" . "In stock";
+                    }
+                    ?>
+                </p>
+                <hr>
+                <p class="product-details"><?php echo "Product details: " . $row["ItemDesc"]?></p>
+                <hr class="hr1">
+                <button class="collapsible"><b>Why buy this phone?</b></button>
+                <div class="content">
+                    <p><?php echo "High quality " . $row["CameraMegapixels"] . " camera"?></p>
+                    <p><?php echo "Long lasting battery life up to " . $row["BatteryLife"] . " hours"?></p>
+                    <p><?php echo "Extremely spacious " .$row["DisplaySize"] . " display for real-screen estate with high screen-to-body ratio"?></p>
+                </div>
+                
+                <button class="collapsible"><b>Information about the phone</b></button>
+                <div class="content">
+                    <p><?php echo "Operating System: " . $row["OperatingSystem"]?></p>
+                    <p><?php echo "Display size: " . $row["DisplaySize"]?></p>
+                    <p><?php echo "Camera: " . $row["CameraMegapixels"]?></p>
+                    <p><?php echo "Biometric authentication: " . $row["BiometricAuthentication"]?></p>
+                    <p><?php echo "Available colours: " . $row["colour"]?></p>
+                    <p><?php echo "Storage space: " . $row["storage"]?></p>
+                    <p><?php echo "Theoretical battery life: " . $row["BatteryLife"] . " hours"?></p>
+                    
+                </div>
+            </div>
         </div>
     </div>
 
-    <div class="row">
-        <div class="col-md-7">
-            <p><?php echo "Details: " . $row["ItemDesc"]?></p>
-            <p>Additional Information</p>
-            <p><?php echo "Price: £" . $row["Price"]?></p>
-            <p><?php echo "Operating System: " . $row["OperatingSystem"]?></p>
-            <p><?php echo "Display size: " . $row["DisplaySize"]?></p>
-            <p><?php echo "Camera: " . $row["CameraMegapixels"]?></p>
-            <p><?php echo "Biometric authentication: " . $row["BiometricAuthentication"]?></p>
-            <p><?php echo "Available colours: " . $row["colour"]?></p>
-            <p><?php echo "Storage space: " . $row["storage"]?></p>
-            <p><?php echo "Theoretical battery life: " . $row["BatteryLife"] . " hours"?></p>
-            <p>
-                <?php
-                /* Get Availability status*/
-                echo "Availability: ";
-                
-                $stock = $row["Stock"]; // get number of items in stock from database
-                
-                /* 
-                * Validate availability status of specific item, i.e. if items are in stock,
-                * otherwise out of stock
-                */
-                if ($stock == 0) {
-                    echo "Out of stock";
+    <script>
+        /* DO NOT REMOVE - controls collapsible elements. If moved to js file, it won't work */
+        /* Iterate through collapsible elements */
+        var coll = document.getElementsByClassName("collapsible");
+        for (var i = 0; i < coll.length; i++) {
+            coll[i].addEventListener("click", function() {
+                this.classList.toggle("active");
+                var content = this.nextElementSibling;
+                if (content.style.maxHeight){
+                    content.style.maxHeight = null;
                 } else {
-                    echo "In stock";
-                }
-                ?>
-            </p>
-            <button class="add-to-basket" >Add To Basket</button>
-        </div>
-    </div>
+                    content.style.maxHeight = content.scrollHeight + "px";
+                } 
+            });
+        }
+    </script>
 </body>
 </html>
