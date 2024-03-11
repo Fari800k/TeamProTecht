64fd812f56a5f96eb5b2616b18934c10df413429
georgesmartx01 <copilaugeorgeandrei87@gmail.com>
2024-03-11 16:12:53 +0000

    update

--- userside/product_dt.php
+++ userside/product_dt.php
@@ -3,21 +3,18 @@
     include "navbar.php";
     include "connectdb.php";
 
-    $Item_ID = ""; 
+    $item_id = ""; 
 
     if(isset($_GET["Item_ID"])) {
-        $Item_ID = $_GET["Item_ID"];
+        $item_id = $_GET["Item_ID"];
     } else {
-       
         echo "Item ID is not set";
         exit(); 
     }
 
     $sql_query = "SELECT * FROM item WHERE Item_ID = ?";
     $ex_query= $pdo->prepare($sql_query);
-    $ex_query->execute([$Item_ID]); 
-
-    
+    $ex_query->execute([$item_id]); 
     $row = $ex_query->fetch(PDO::FETCH_ASSOC);
 ?>
 
@@ -34,7 +31,7 @@
 
 <body>
     <div class="row">
-        <h1><?php echo $row["ItemName"]. ""?></h1>
+        <h1><?php echo $row["ItemName"] . " - " . $row["ItemDesc"]?></h1>
         <div class="column">
             <div class="card">
                 <img class="phone_image" src="CSS/images/<?php echo $row["Img"]; ?>"/>
@@ -44,7 +41,7 @@
         <div class="column">
             <div class="card">
                 <p class="price"><b><?php echo "£" . $row["Price"]?></b></p>
-                <button class="add-to-basket"><b>Add to Basket</b></button>
+                <button class="add-to-basket" onclick="redirectToBasketPage()"><b>Add to Basket</b></button>
                 <p class="stock">
                     <?php
                     /* Get Availability status*/
@@ -65,25 +62,25 @@
                     ?>
                 </p>
                 <hr>
-                <p class="product-details"><?php echo "Product details: " . $row["ItemDesc"]?></p>
-                <hr class="hr1">
+                <p class="product-details"><?php echo "<b>Short description:</b> " . $row["ItemDesc"]?></p>
+                <hr>
                 <button class="collapsible"><b>Why buy this phone?</b></button>
                 <div class="content">
-                    <p><?php echo "High quality " . $row["CameraMegapixels"] . " camera"?></p>
+                    <p><?php echo "High quality " . $row["CameraMegapixels"] . " camera for excellent pictures"?></p>
                     <p><?php echo "Long lasting battery life up to " . $row["BatteryLife"] . " hours"?></p>
-                    <p><?php echo "Extremely spacious " .$row["DisplaySize"] . " display for real-screen estate with high screen-to-body ratio"?></p>
+                    <p><?php echo "Large and spacious " .$row["DisplaySize"] . " display for real-screen estate with high screen-to-body ratio"?></p>
+                    <p><?php echo "High storage capacity for storing your favourite pictures and videos, and other things you love and enjoy " ?></p>
                 </div>
                 
-                <button class="collapsible"><b>Information about the phone</b></button>
+                <button class="collapsible"><b>Product information</b></button>
                 <div class="content">
-                    <p><?php echo "Operating System: " . $row["OperatingSystem"]?></p>
-                    <p><?php echo "Display size: " . $row["DisplaySize"]?></p>
-                    <p><?php echo "Camera: " . $row["CameraMegapixels"]?></p>
-                    <p><?php echo "Biometric authentication: " . $row["BiometricAuthentication"]?></p>
-                    <p><?php echo "Available colours: " . $row["colour"]?></p>
-                    <p><?php echo "Storage space: " . $row["storage"]?></p>
-                    <p><?php echo "Theoretical battery life: " . $row["BatteryLife"] . " hours"?></p>
-                    
+                    <p><?php echo "<b>Operating System</b>: " . $row["OperatingSystem"]?></p>
+                    <p><?php echo "<b>Display size</b>: " . $row["DisplaySize"]?></p>
+                    <p><?php echo "<b>Camera</b>: " . $row["CameraMegapixels"]?></p>
+                    <p><?php echo "<b>Biometric authentication:</b> " . $row["BiometricAuthentication"]?></p>
+                    <p><?php echo "<b>Available colours:</b> " . $row["colour"]?></p>
+                    <p><?php echo "<b>Storage space:</b> " . $row["storage"]?></p>
+                    <p><?php echo "<b>Theoretical battery life:</b> " . $row["BatteryLife"] . " hours"?></p>
                 </div>
             </div>
         </div>
@@ -103,6 +100,10 @@
                     content.style.maxHeight = content.scrollHeight + "px";
                 } 
             });
+        }
+
+        function redirectToBasketPage() {
+            location.replace("addtobasket.php");
         }
     </script>
 </body>
