78f610d3ff414bfd94d479eb70777c43151cd006
LouieBWC <114592652+LouieBWC@users.noreply.github.com>
2024-03-15 15:10:27 +0000

    Update footer.php

--- userside/footer.php
+++ userside/footer.php
@@ -7,27 +7,21 @@
     <link rel="stylesheet" href="CSS/footer.css">
 </head>
 <body>
-
-<footer>
-    <div class="footer_content">
-        <div class="social_media">
-            <h3>Social Media</h3>
-            <ul class="social-icons">
-                <li><a href="https://en-gb.facebook.com/" target="_blank"><img src="CSS/images/facebook.png" alt="Facebook" width="50px" height="50px"></a></li>
-                <li><a href="https://twitter.com/i/flow/login" target="_blank"><img src="CSS/images/twitter.png" alt="Twitter" width="50px" height="50px"></a></li>
-                <li><a href="https://www.instagram.com/accounts/login/" target="_blank"><img src="CSS/images/insta.png" alt="Instagram" width="50px" height="50px"></a></li>
-            </ul>
+    <footer>
+        <div class="footer_content">
+            <div class="social_media">
+                <ul class="social-icons">
+                    <li><a href="https://en-gb.facebook.com/" target="_blank"><img src="CSS/images/facebook.png" alt="Facebook"></a></li>
+                    <li><a href="https://twitter.com/i/flow/login" target="_blank"><img src="CSS/images/twitter.png" alt="Twitter"></a></li>
+                    <li><a href="https://www.instagram.com/accounts/login/" target="_blank"><img src="CSS/images/insta.png" alt="Instagram"></a></li>
+                </ul>
+            </div>
+            <div class="contact_info">
+                <p>Email: group3.cs2tp@gmail.com</p>
+                <p>Phone: +44 (0) 123456789</p>
+            </div>
         </div>
-
-        <div class="contact_info">
-            <h3>Contact</h3>
-            <p>Email: group3.cs2tp@gmail.com</p>
-            <p>Phone: +44 (0) 123456789</p>
-        </div>
-    </div>
-
-    <p class="copyright">&copy; <?php echo date("Y"); ?> Phone Store. All rights reserved.</p>
-</footer>
-
+        <p class="copyright">&copy; <?php echo date("Y"); ?> Phone Store. All rights reserved.</p>
+    </footer>
 </body>
 </html>
