22417e21d7e3fe6f100c8fef0170fe145d564991
mmh24 <149673566+mmh24@users.noreply.github.com>
2024-03-16 06:58:42 +0000

    updated footer code

--- userside/footer.php
+++ userside/footer.php
@@ -10,15 +10,35 @@
     <footer>
         <div class="footer_content">
             <div class="social_media">
+                <h3>Social Media</h3>
                 <ul class="social-icons">
                     <li><a href="https://en-gb.facebook.com/" target="_blank"><img src="CSS/images/facebook.png" alt="Facebook"></a></li>
                     <li><a href="https://twitter.com/i/flow/login" target="_blank"><img src="CSS/images/twitter.png" alt="Twitter"></a></li>
                     <li><a href="https://www.instagram.com/accounts/login/" target="_blank"><img src="CSS/images/insta.png" alt="Instagram"></a></li>
                 </ul>
+                <p>Connect with us on social media for updates and news!</p>
             </div>
             <div class="contact_info">
-                <p>Email: group3.cs2tp@gmail.com</p>
-                <p>Phone: +44 (0) 123456789</p>
+                <h3>Contact Info</h3>
+                <p>Email: <a href="mailto:group3.cs2tp@gmail.com">group3.cs2tp@gmail.com</a></p>
+                <p>Phone: <a href="tel:+44123456789">+44 (0) 123456789</a></p>
+                <p>You can reach out to us via email or phone for any inquiries.</p>
+            </div>
+            <div class="countries">
+                <h3>Operating Countries</h3>
+                <ul>
+                <li><a href="">United States</a></li>
+                    <li><a href="">United Kingdom</a></li>
+                    <li><a href="">Canada</a></li>
+                    <li><a href="">Germany</a></li>
+                    <li><a href="">France</a></li>
+                    <li><a href="">Australia</a></li>
+                    <li><a href="">Japan</a></li>
+                    <li><a href="">China</a></li>
+                    <li><a href="">India</a></li>
+                    <li><a href="">Brazil</a></li>
+                </ul>
+                <p>We operate in these 10 countries.</p>
             </div>
         </div>
         <p class="copyright">&copy; <?php echo date("Y"); ?> Phone Store. All rights reserved.</p>
