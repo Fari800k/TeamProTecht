d2724a0804e84244c747b95ef0b44d3b99de259a
DanAlexCox <adala738@gmail.com>
2024-03-16 01:02:41 +0000

    Incomplete rating system

---
+++ JavaScript/script.js
@@ -0,0 +1,99 @@
+document.addEventListener("DOMContentLoaded", function(){
+    var page = document.querySelector("html");
+    var basketNav = document.getElementById("buttonbasket");
+    var basketNavView = document.getElementById("mybasketdropdown");
+    var coll = document.getElementsByClassName("collapsible");
+
+    page.addEventListener("click", function(event){
+        if(basketNavView.style.display === "flex"){
+            basketNavView.style.display = "none";
+            event.stopPropagation();
+        }
+    });
+
+    basketNav.addEventListener("mouseover", function(){
+        if(basketNavView.style.display !== "flex"){
+            basketNavView.style.display = "flex";
+        }
+    });
+
+    for (var i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.maxHeight){
+                content.style.maxHeight = null;
+            } else {
+                content.style.maxHeight = content.scrollHeight + "px";
+            } 
+        });
+    }
+
+    document.addEventListener("DOMContentLoaded", function(){
+        var page = document.querySelector("html");
+        var accountNav = document.getElementById("buttonaccount");
+        var accountNavView = document.getElementById("myaccountdropdown");
+
+        page.addEventListener("click", function(event){
+            if(accountNavView.style.display === "block"){
+                accountNavView.style.display = "none";
+                event.stopPropagation();
+            }
+        });
+
+        accountNav.addEventListener("mouseover", function(){
+            if(accountNavView.style.display !== "block"){
+                accountNavView.style.display = "block";
+            }
+        });
+    });
+});
+
+function priceChange(){
+    const min = document.getElementsByName('minprice');
+    const max = document.getElementsByName('maxprice');
+
+    if(min >= max){
+        document.getElementsByID("myPrices").innerHTML = "Min price needs to be smaller than max price.";
+    }
+}
+
+//stored search item gets put back into search bar
+function getSearch(){
+    localStorage.getItem('search');
+}
+
+function resetFilters() {
+    window.location.href = 'browse.php';
+}
+
+function notLoggedIn(){
+    alert(window.location('accountlogin.php'));
+}
+
+// Function to open the full popup menu
+function openPopupMenu() {
+    document.querySelector('.popup-menu-overlay').style.display = 'block';
+    document.querySelector('.popup-menu').style.display = 'block';
+}
+
+// Function to close the full popup menu
+function closePopupMenu() {
+    document.querySelector('.popup-menu-overlay').style.display = 'none';
+    document.querySelector('.popup-menu').style.display = 'none';
+}
+
+function registerPopup(){
+    document.getElementById('createUserPopup').style.display='block';
+}
+
+function cancelRegisterPopup(){
+    document.getElementById('createUserPopup').style.display='none';
+}
+
+function registerButton() {
+    var popup = document.getElementById('createUserPopup');
+    if(popup !== "block"){
+        popup.style.display = "block";
+    }
+}
---
+++ accountpage.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Account Page</title>
+    <link rel="stylesheet" type="text/css" href="CSS/account.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script defer src="JavaScript/script.js"></script>
+
+</head>
+<?php
+include('connectdb.php');
+session_start();
+include "navbar.php";
+if (!isset($_SESSION['User_ID'])) {
+    header("Location: login.php");
+}
+//retrieve the orders for the current user
+$sql = "SELECT orders.Order_ID, orders.Order_Status, orders.Address_Order
+        FROM orders
+        INNER JOIN basket ON orders.Basket_ID = basket.Basket_ID
+        WHERE basket.User_ID = :userID";
+$stmt = $pdo->prepare($sql);
+$stmt->bindParam(':userID', $_SESSION['User_ID'], PDO::PARAM_INT);
+$stmt->execute();
+$orders = $stmt->fetchAll();
+
+//form submission
+if (isset($_POST['start_return'])) {
+    $orderID = $_POST['order_id'];
+
+    //update status
+    $sql = 'UPDATE orders SET Order_Status = "Returning" WHERE Order_ID = :orderID';
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':orderID', $orderID, PDO::PARAM_INT);
+
+    if ($stmt->execute()) {
+        echo "Order status updated to 'Return Started' successfully.";
+    } else {
+        echo "Error updating order status.";
+    }
+}
+?>
+
+<body>
+    <div class="accountbody">
+            <div class="card">
+                <img class="user_image" src="CSS/images/userlogo.png"/>
+                <h3>
+                    <?php echo $_SESSION['username']; ?>
+                </h3>
+                <a href="">Update Details</a><br>
+                <a href="" onclick="returnPopup()">Return an Order</a><br>
+                <a href="" >Review a product</a>
+                <div id="returnOrderPopup" class="popup-menu">
+                    <h3>Return Order</h3>
+                    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+                        <label for="order_id">Select Order:</label>
+                        <select id="order_id" name="order_id">
+                            <?php foreach ($orders as $order): ?>
+                                <option value="<?php echo $order['Order_ID']; ?>">
+                                    Order ID:
+                                    <?php echo $order['Order_ID']; ?> (
+                                    <?php echo $order['Order_Status']; ?>)
+                                </option>
+                            <?php endforeach; ?>
+                        </select>
+                        <br>
+                        <br>
+                        <br>
+                        <input type="submit" name="start_return" value="Start Return">
+                        <br>
+                        <br>
+                        <button type="button" onclick="cancelReturnPopup()">Cancel</button>
+                    </form>
+                </div>
+            </div>
+            <?php
+            try {
+                $order_sql = "SELECT orderbaskets.Order_ID, orderbaskets.Basket_ID,
+                                brand.BrandName, item.Item_ID, item.ItemName, item.Img, basketitem.Quantity,
+                                orderbaskets.Order_Status, (basketitem.Quantity * item.Price) AS Total_Price
+                            FROM basketitem
+                            JOIN ((SELECT orders.Order_ID, orders.Order_Status, basket.Basket_ID, basket.User_ID
+                                FROM orders JOIN basket ON basket.Basket_ID = orders.Basket_ID
+                                WHERE basket.User_ID = :user_id) AS orderbaskets)
+                            ON orderbaskets.Basket_ID = basketitem.Basket_ID
+                            JOIN item ON item.Item_ID = basketitem.Item_ID
+                            JOIN brand ON brand.Item_ID = basketitem.Item_ID";
+                
+                $order_stmt = $pdo->prepare($order_sql);
+                $order_stmt->bindParam(':user_id', $_SESSION['User_ID'], PDO::PARAM_INT);
+                $order_stmt->execute();
+
+                $currentorderbasket = null;
+                $totalbasketprice = 0;
+
+                echo '<div class="prev">';
+                echo "<h3>Previous Orders</h3>";
+                
+                foreach ($order_stmt as $order) {
+                    $orderid = $order['Order_ID'];
+                    $basketid = $order['Basket_ID'];
+                    $itemid = $order['Item_ID'];
+                    $brandname = $order['BrandName'];
+                    $itemname = $order['ItemName'];
+                    $itemimg = $order['Img'];
+                    $itemquantity = $order['Quantity'];
+                    $totalitemprice = $order['Total_Price'];
+                    $orderStatus = $order['Order_Status'];
+
+                    echo "<div class='order'>";
+                    echo '<div class="phone">';
+                    echo '<h4>' . 'Order #:' . $orderid . '</h4>';
+                    echo '<img src="CSS/images/' . $itemimg . '" alt="asus" class="phone">';
+                    echo '<div class="phone_name">';
+                    echo "<h4>" . $brandname . " " . $itemname . "</h4>";
+                    echo "<h4>" . 'Quantity: ' . $itemquantity . "</h4>";
+                    echo "<h4>" . 'Total Price £' . $totalitemprice . "</h4>";
+                    echo "<h4>" . 'Order Status: ' . $orderStatus . "</h4>";
+                    echo "</div>";
+                    echo "</div>";
+                    ?>
+                    <!-- make review form -->
+                    <div class='review'>
+                        <form method ='post' id="<?php echo $orderid."-".$itemid; ?>" class='reviewform' action='reviewitem.php'>
+                            <h4>Rating</h4>
+                            <span onclick="star(1)" class="fa fa-star" value="1"></span>
+                            <span onclick="star(2)" class="fa fa-star" value="2"></span>
+                            <span onclick="star(3)" class="fa fa-star" value="3"></span>
+                            <span onclick="star(4)" class="fa fa-star" value="4"></span>
+                            <span onclick="star(5)" class="fa fa-star" value="5"></span><br>
+                            <input type='hidden' id='stars' name="rating" value="0">
+                            <h4>Share your thoughts below</h4>
+                            <input type='text' name='reviewdesc'>
+                            <input type='hidden' name='itemid' value='<?php echo $itemid ?>'>
+                            <input type='submit' name='reviewsubmit'></button>
+                        </form>
+                    </div>
+                    <?php
+                    echo "</div>";
+
+                    $totalbasketprice += $totalitemprice;
+                }
+                echo "</div>";
+
+            } catch (Exception $e) {
+                echo 'Message: ' . $e->getMessage();
+            }
+            ?>
+            <script>
+                function returnPopup() {
+                    document.getElementById('returnOrderPopup').style.display = 'block';
+                }
+
+                function cancelReturnPopup() {
+                    document.getElementById('returnOrderPopup').style.display = 'none';
+                }
+
+                let stars = document.getElementsByClassName("fa fa-star");
+                let output = document.getElementById("stars");
+
+                function star(n){
+                    remove();
+                    for (let i = 0; i < n; i++) {
+                        if (n == 1) cls = "one";
+                        else if (n == 2) cls = "two";
+                        else if (n == 3) cls = "three";
+                        else if (n == 4) cls = "four";
+                        else if (n == 5) cls = "five";
+                        stars[i].className = "fa fa-star " + cls;
+                    }
+                output.value = n;
+                }
+
+                function remove() {
+                    let i = 0;
+                    while (i < 5) {
+                        stars[i].className = "fa fa-star";
+                        i++;
+                    }
+                }
+            </script>
+</body>
