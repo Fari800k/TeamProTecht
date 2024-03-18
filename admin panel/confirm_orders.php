80f91d689a08c36f1cd627f75d76a6b462faf1a3
HuseinDawood <h.dawood1360@gmail.com>
2024-03-18 16:42:36 +0000

    updated css reference

--- admin panel/confirm_orders.php
+++ admin panel/confirm_orders.php
@@ -2,7 +2,7 @@
     <head>
             <!---box icons css-->
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    <link rel="stylesheet" href="employee.css">
+    <link rel="stylesheet" href="stocksystem.css">
 </head>
     
 
