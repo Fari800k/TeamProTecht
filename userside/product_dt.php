2cd5d5fb24f8020c9b724994a04944752c091c80
georgesmartx01 <copilaugeorgeandrei87@gmail.com>
2024-03-20 16:08:02 +0000

    updated

--- userside/product_dt.php
+++ userside/product_dt.php
@@ -14,15 +14,19 @@
 
     $item_query = "SELECT * FROM item WHERE Item_ID = ?";
     $brand_query = "SELECT * FROM brand";
+    $review_query = "SELECT * FROM reviews";
 
-    $ex_query= $pdo->prepare($item_query);
-    $exec = $pdo->prepare($brand_query);
+    $ex_item= $pdo->prepare($item_query);
+    $ex_brand = $pdo->prepare($brand_query);
+    $ex_rev = $pdo->prepare($review_query);
     
-    $ex_query->execute([$item_id]);
-    $exec->execute();
+    $ex_item->execute([$item_id]);
+    $ex_brand->execute();
+    $ex_rev->execute();
 
-    $row = $ex_query->fetch(PDO::FETCH_ASSOC);
-    $brand_row = $exec->fetch(PDO::FETCH_ASSOC);
+    $item_row = $ex_item->fetch(PDO::FETCH_ASSOC);
+    $brand_row = $ex_brand->fetch(PDO::FETCH_ASSOC);
+    $rev_row = $ex_rev->fetch(PDO::FETCH_ASSOC);
 ?>
 
 <!DOCTYPE html>
@@ -31,7 +35,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Title of the item -->
-    <title><?php echo $brand_row["BrandName"] . " " . $row["ItemName"]?></title>
+    <title><?php echo $brand_row["BrandName"] . " " . $item_row["ItemName"]?></title>
     <link rel="stylesheet" href="CSS/product_dt.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script defer src="JavaScript/script.js"></script>
@@ -44,19 +48,19 @@
     include "navbar.php";
     ?>
     <div class="row">
-        <h1 class="item-name"><?php echo $brand_row["BrandName"] . " " . $row["ItemName"]?></h1>
+        <h1 class="item-name"><?php echo $brand_row["BrandName"] . " " . $item_row["ItemName"]?></h1>
         <div id="phone_image_column" class="column">
-            <img class="phone_image" src="CSS/images/<?php echo $row["Img"]?>"/>        
+            <img class="phone_image" src="CSS/images/<?php echo $item_row["Img"]?>"/>        
         </div>
 
         <div id = "phone_info_column" class="column">
-            <p class="price"><b><?php echo "£" . $row["Price"] ?></b></p>
+            <p class="price"><b><?php echo "£" . $item_row["Price"] ?></b></p>
             <hr>
             <!-- TASK FOR DANIEL: MAKE FORM FOR ADD TO BASKET -->
             <p>
                 <?php
                 /* Get Availability status */
-                $stock = $row["Stock"]; // get number of items in stock from database
+                $stock = $item_row["Stock"]; // get number of items in stock from database
                 
                 /* 
                 * Validate availability status for specific item
@@ -66,7 +70,7 @@
                     $error_msg = "Out of stock";
                     echo "<i id='cross' class='fa fa-times'></i>" . "<strong>" .$error_msg. "</strong>";
                 } else if ($stock == 1) {
-                    $critical_msg = "This is our last product, if you want it! Hurry before someone else buys it!" ;
+                    $critical_msg = "This is our last product, if you want it! Hurry before it's gone!" ;
                     echo "<i id='warning' class='fa fa-exclamation'></i>" . "<strong>" .$critical_msg. "</strong>";
                 } else if ($stock >=2 and $stock <=5) {
                     $more_prompt_msg = "Stock is close to running out!";
@@ -84,25 +88,25 @@
             <hr>
             <button class="collapsible"><b>Brief Product Description</b></button>
             <div class="content">
-                <p><?php echo $row["ItemDesc"] ?></p>
+                <p><?php echo $item_row["ItemDesc"] ?></p>
             </div>
             
             <button class="collapsible"><b>Why buy this phone?</b></button>
             <div class="content">
-                <p><?php echo "High quality " . $row["CameraMegapixels"] . " camera"?></p>
-                <p><?php echo "Long lasting battery life up to " . $row["BatteryLife"] . " hours"?></p>
-                <p><?php echo "Extremely spacious " .$row["DisplaySize"] . " display for real-screen estate with high screen-to-body ratio"?></p>
+                <p><?php echo "High quality " . $item_row["CameraMegapixels"] . " camera"?></p>
+                <p><?php echo "Long lasting battery life up to " . $item_row["BatteryLife"] . " hours"?></p>
+                <p><?php echo "Extremely spacious " .$item_row["DisplaySize"] . " display for real-screen estate with high screen-to-body ratio"?></p>
             </div>
             
             <button class="collapsible"><b>Phone Specs</b></button>
             <div class="content">
-                <p><?php echo "<b>Operating System:</b> " . $row["OperatingSystem"]?></p>
-                <p><?php echo "<b>Display size:</b> " . $row["DisplaySize"]?></p>
-                <p><?php echo "<b>Camera:</b> " . $row["CameraMegapixels"]?></p>
-                <p><?php echo "<b>Biometric authentication:</b> " . $row["BiometricAuthentication"]?></p>
-                <p><?php echo "<b>Available colour(s):</b> " . $row["colour"]?></p>
-                <p><?php echo "<b>Storage space:</b> " . $row["storage"]?></p>
-                <p><?php echo "<b>Theoretical battery life:</b> " . $row["BatteryLife"] . " hours"?></p>
+                <p><?php echo "<b>Operating System:</b> " . $item_row["OperatingSystem"]?></p>
+                <p><?php echo "<b>Display size:</b> " . $item_row["DisplaySize"]?></p>
+                <p><?php echo "<b>Camera:</b> " . $item_row["CameraMegapixels"]?></p>
+                <p><?php echo "<b>Biometric authentication:</b> " . $item_row["BiometricAuthentication"]?></p>
+                <p><?php echo "<b>Available colour(s):</b> " . $item_row["colour"]?></p>
+                <p><?php echo "<b>Storage space:</b> " . $item_row["storage"]?></p>
+                <p><?php echo "<b>Theoretical battery life:</b> " . $item_row["BatteryLife"] . " hours"?></p>
             </div>            
         </div>   
             
@@ -124,42 +128,20 @@
             <div id="rev_col" class="column">
             <h1 class="rev">What people have said about this product?</h1>
                 <div id="rev_card" class="card">
-                    <?php
-                    $user_query = "SELECT User_ID, Username FROM users";
-                    
-
-                    $rev_query = "SELECT User_ID, Item_ID, Rating, Description, Created_at FROM reviews";     
-                    $exct = $pdo->prepare($rev_query);
-                    $exct->execute();
-                    $rev_row = $exct->fetch(PDO::FETCH_ASSOC);
-                    ?>
 
                     <div class="row">
                         <div class="side">
                             <div>
-                                <?php 
-                                
-                                
+                                <?php
+                                for () {
+                                    
+                                }
                                 echo $rev_row["Description"]
-                                
-                                ?></div>
-                        </div>
-
-                        <div class="middle">
-                            <div class="bar-container">
-                                <div class="bar">
-                                </div>
+                                ?>
                             </div>
-
-                            <div class="side right">
-                                <div>150</div>
-                            </div>
-
-
                         </div>
                     </div>
                 </div>
-
             </div>
         </div>
     </div>
