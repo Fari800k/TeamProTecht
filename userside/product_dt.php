3ad3f21229219216b2d06ef93eb197e690beb04e
georgesmartx01 <copilaugeorgeandrei87@gmail.com>
2024-03-22 12:43:54 +0000

    update

--- userside/product_dt.php
+++ userside/product_dt.php
@@ -49,13 +49,13 @@
     include "navbar.php";
     ?>
     <div class="row">
-        <h1 class="item-name"><?php echo $brand_row["BrandName"] . " " . $item_row["ItemName"]?></h1>
+        <h1 class="item-name"><?php echo $brand_row["BrandName"] . " " .$item_row["ItemName"]?></h1>
         <div id="phone_image_column" class="column">
             <img class="phone_image" src="CSS/images/<?php echo $item_row["Img"]?>"/>        
         </div>
 
         <div id = "phone_info_column" class="column">
-            <p class="price"><b><?php echo "£" . $item_row["Price"] ?></b></p>
+            <p class="price"><b><?php echo "£" .$item_row["Price"] ?></b></p>
             <hr>
             <p>
                 <?php
@@ -93,21 +93,21 @@
             
             <button class="collapsible"><b>Why buy this phone?</b></button>
             <div class="content">
-                <p><?php echo "High quality " . $item_row["CameraMegapixels"] . " camera"?></p>
-                <p><?php echo "Long lasting battery life up to " . $item_row["BatteryLife"] . " hours"?></p>
-                <p><?php echo "Extremely spacious " .$item_row["DisplaySize"] . " display for real-screen estate with high screen-to-body ratio"?></p>
+                <p><?php echo "High quality " .$item_row["CameraMegapixels"]. " camera"?></p>
+                <p><?php echo "Long lasting battery life up to " .$item_row["BatteryLife"]. " hours"?></p>
+                <p><?php echo "Extremely spacious " .$item_row["DisplaySize"]. " display for real-screen estate with high screen-to-body ratio"?></p>
             </div>
             
             <button class="collapsible"><b>Phone Specs</b></button>
             <div class="content">
-                <p><?php echo "<b>Operating System:</b> " . $item_row["OperatingSystem"]?></p>
-                <p><?php echo "<b>Display size:</b> " . $item_row["DisplaySize"]?></p>
-                <p><?php echo "<b>Camera:</b> " . $item_row["CameraMegapixels"]?></p>
-                <p><?php echo "<b>Biometric authentication:</b> " . $item_row["BiometricAuthentication"]?></p>
-                <p><?php echo "<b>Available colour(s):</b> " . $item_row["colour"]?></p>
-                <p><?php echo "<b>Storage space:</b> " . $item_row["storage"]?></p>
-                <p><?php echo "<b>Theoretical battery life:</b> " . $item_row["BatteryLife"] . " hours"?></p>
-            </div>            
+                <p><?php echo "<b>Operating System:</b> ".$item_row["OperatingSystem"]?></p>
+                <p><?php echo "<b>Display size:</b> " .$item_row["DisplaySize"]?></p>
+                <p><?php echo "<b>Camera:</b> " .$item_row["CameraMegapixels"]?></p>
+                <p><?php echo "<b>Biometric authentication:</b> " .$item_row["BiometricAuthentication"]?></p>
+                <p><?php echo "<b>Available colour(s):</b> " .$item_row["colour"]?></p>
+                <p><?php echo "<b>Storage space:</b> " .$item_row["storage"]?></p>
+                <p><?php echo "<b>Theoretical battery life:</b> " .$item_row["BatteryLife"] . " hours"?></p>
+            </div>
         </div>   
             
         <?php
@@ -126,55 +126,58 @@
             <h1 class="best-sellers">Best Sellers</h1>
             <?php include "featureditem.php"?>
             <div id="rev_col" class="column">
-            <hr>
+            <br><br><hr>
             <h1 class="rev">What people have said about this product?</h1>
                 <div id="rev_card" class="card">
                     <p class="summary"><b>Summary of rating reviews</b></p>
                     <div class="row">
                         <?php
+                        include "SQL/connectdb.php";
+
                         /* Display all five stars */
                         for ($num_stars = 1; $num_stars <= 5; $num_stars++) {
                             echo "<span style='float: left; font-size: 25px;' class='fa fa-star'></span>";  
                         }
 
                         echo "<p style = 'transform: translate(15px, 2px)'>Average rating of ". " out of 5 stars</p>";
+                        
+                        echo "<br>";
+
                         /* Iterate through scorecard to display it
                          * Review from user(s) is/are also displayed (icon is also shown)
-                        */
-                        echo "<br>";
+                         * Display the stars with the average rating
+                         */
                         for ($num = 1; $num <= 5; $num++) {
-                            echo "<br>";
+                            
                             echo "<div class='side'><p style='text-align: left'>" .$num. " star" . "</p></div>";
-                            echo "<div class='middle'><div class='bar-container' style='background-color: #f1f1f1; width: 5%; text-align: center;'>
-                            <div class='bar-$num' style='height: 18px; background-color: #04AA6D;'></div></div></div>";
+                            echo "<div class='middle'><div class='bar-container' style='background-color: #d1d1d1; width: 95%; text-align: center;'>
+                            <div class='bar-$num' style='height: 18px;'></div></div></div>";
                             echo "<div class='side right'></div>";
                         }
-                        
-                        ?>
 
-                        <div>
-                        <?php
-                        include "SQL/connectdb.php";
                         $review_query = "SELECT * FROM reviews WHERE Item_ID = $item_id";
-                        $user_query = "SELECT * FROM users ";
+                        $user_query = "SELECT * FROM users WHERE Username = $username";
                         $result_review = mysqli_query($con, $review_query);
-                        $result_user = mysqli_query($con, $user_query);
-
+                        
+                        echo "<br>";
                         echo "<p style='text-align: left; font-size: 24px;'><b>User reviews</b></p>";
-
                         while ($review_row = mysqli_fetch_array($result_review)) {
                             $desc = $review_row["Description"];
-                            echo "<br><i style='font-size: 40px; float: left; margin-top: -8px; margin-right: 5px;' class='fa fa-user'>" . "</i>";
+                            $rating = array($review_row["Rating"]);
+                            
+                            echo "<br><i style='font-size: 40px; float: left; margin-top: -8px; margin-right: 5px;' class='fa fa-user'></i>";
+                            echo "<br><br>";
 
-                            for ($num_stars = 1; $num_stars <= 5; $num_stars++) {
+                            /* Display the stars with the given user rating*/
+                            for ($num_of_stars = 1; $num_of_stars <= 5; $num_of_stars++) {
                                 echo "<span style='float: left; font-size: 25px;' class='fa fa-star'></span>";
                             }
 
-                            echo "<br><br>";
-                            echo "<p style='text-align: left'>" .$desc . "</p>";
+                            echo "<br>";
+                            echo "<p style='text-align: left'>" .$desc. "</p>";
+                            echo "<p style='text-align: left'>" .array_sum($rating). "</p>";
                         }
                         ?>
-                    </div>
                 </div>
             </div>
         </div>
